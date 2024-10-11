f17e7fbfec578f5152378368732cddb28c8de6b2
Joao Ferreira <joaovitorferreira03@gmail.com>
2024-10-11 12:39:11 -0300

    feat: adding api keys

--- xfact-metamorphic-tests/utils/api.py
+++ xfact-metamorphic-tests/utils/api.py
@@ -2,9 +2,12 @@ import maritalk
 import openai
 import google.generativeai as genai
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def gpt_request(prompt):
-    openai.api_key = ""
+    openai.api_key = os.getenv("OPEN_API_KEY")
     response = openai.Completion.create(
         model="gpt-4o-mini",
         prompt=prompt,
@@ -13,13 +16,13 @@ def gpt_request(prompt):
     return response['choices'][0]['text'].strip()
 
 def maritaca_request(prompt):
-    model = maritalk.MariTalk(key="api key", model="sabia-2-small")
+    model = maritalk.MariTalk(key=os.getenv("MARITACA_API_KEY"), model="sabia-2-small")
     response = model.generate(prompt, max_tokens=200)
     answer = response["answer"]
     return answer
 
 def gemini_request(prompt):
-    genai.configure(api_key="")
+    genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
     model = genai.GenerativeModel("gemini-1.5-flash")
     response = model.generate_content(prompt)
     return response.text
